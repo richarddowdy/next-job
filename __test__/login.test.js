dfe7be3029a78e082784230c6201d5a1a3ee13dc
Richard Dowdy <richard.dowdy0@gmail.com>
2021-06-22 19:42:56 -0700

    added auth and testing for auth

---
+++ __test__/login.test.js
@@ -0,0 +1,40 @@
+import login from "../pages/api/login";
+import { createMocks } from "node-mocks-http";
+import prisma from "../lib/prisma";
+import "setimmediate";
+
+afterEach(async (done) => {
+  await prisma.$disconnect();
+  done();
+});
+
+describe("/POST /api/login", () => {
+  test("/login responds 400 to GET", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+      query: {
+        username: "test",
+        password: "password",
+      },
+    });
+
+    await login(req, res);
+
+    expect(res._getStatusCode()).toBe(400);
+  });
+
+  test("/login responds with JWt", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: {
+        username: "checffeceece",
+        password: "password",
+      },
+    });
+
+    await login(req, res);
+
+    expect(res._getStatusCode()).toBe(200);
+    // expect(JSON.parse(res._getData());
+  });
+});
